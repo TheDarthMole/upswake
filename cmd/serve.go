d1223b533b0a431ae266e42a7712899df81acdb2
Nick Ruffles <nick.ruffles@engineerbetter.com>
2023-10-08 17:42:25 +0100

    chore: remove test code

--- cmd/serve.go
+++ cmd/serve.go
@@ -2,9 +2,6 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
-	"log"
-	"upsWake/network"
-	"upsWake/wol"
 )
 
 var (
@@ -13,18 +10,6 @@ var (
 )
 
 func init() {
-	bc, err := network.GetAllBroadcastAddresses()
-	if err != nil {
-		panic(err)
-	}
-	stringBroadcasts := network.IPsToStrings(bc)
-	serveCmd.Flags().StringArrayVarP(&broadcasts, "broadcasts", "b", stringBroadcasts, "Broadcast addresses to send the WoL packet to, e.g. 192.168.1.255,172.16.0.255")
-	serveCmd.Flags().StringVarP(&mac, "mac", "m", "", "MAC address of the computer to wake")
-	err = serveCmd.MarkFlagRequired("mac")
-	if err != nil {
-		log.Println("baffled")
-		return
-	}
 	rootCmd.AddCommand(serveCmd)
 }
 
@@ -33,14 +18,6 @@ var serveCmd = &cobra.Command{
 	Short: "Run the UPSWake server",
 	Long:  `All software has versions. This is Hugo's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		ipBroadcasts, err := network.StringsToIPs(broadcasts)
-		log.Println(ipBroadcasts)
-		if err != nil {
-			panic(err)
-		}
-		err = wol.Wake(mac, ipBroadcasts)
-		if err != nil {
-			log.Panic(err)
-		}
+
 	},
 }
