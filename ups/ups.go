2aaf724219fed769c73005f966f51b6cbe6dab20
Nick Ruffles <nick.ruffles@engineerbetter.com>
2023-10-16 15:05:21 +0100

    chore: refactor ups getters

--- ups/ups.go
+++ ups/ups.go
@@ -1,6 +1,7 @@
 package ups
 
 import (
+	"encoding/json"
 	"fmt"
 	nut "github.com/robbiet480/go.nut"
 	"log"
@@ -36,18 +37,41 @@ func (u *UPS) GetBatteryCharge(upsName string) (int64, error) {
 }
 
 func (u *UPS) getValueFromUPS(upsName, variableName string) (interface{}, error) {
-	list, err := u.GetUPSList()
+	ups, err := u.getUPSFromList(upsName)
 	if err != nil {
 		return "", err
 	}
-	for _, ups := range list {
-		if ups.Name == upsName {
-			for _, variable := range ups.Variables {
-				if variable.Name == variableName {
-					return variable.Value, nil
-				}
-			}
+	for _, variable := range ups.Variables {
+		if variable.Name == variableName {
+			return variable.Value, nil
 		}
 	}
 	return "", fmt.Errorf("could not find UPS with name '%s'", upsName)
+}
+
+func (u *UPS) getUPSFromList(upsName string) (nut.UPS, error) {
+	list, err := u.GetUPSList()
+	if err != nil {
+		return nut.UPS{}, fmt.Errorf("could not get UPS list: %w", err)
+	}
+	for _, ups := range list {
+		if ups.Name == upsName {
+			return ups, nil
+		}
+	}
+	return nut.UPS{}, fmt.Errorf("could not find UPS with name '%s'", upsName)
+}
+
+func (u *UPS) ToJson() (string, error) {
+	upss, err := u.GetUPSList()
+	if err != nil {
+		return "", err
+	}
+
+	jsonData, err := json.Marshal(upss)
+	if err != nil {
+		return "", err
+	}
+
+	return string(jsonData), err
 }
