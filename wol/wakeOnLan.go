eef6990af1ed95394dee36f4ab23c268e0b110ef
Nick Ruffles <nick.ruffles@engineerbetter.com>
2023-10-17 22:29:38 +0100

    wip: add better tests

--- wol/wakeOnLan.go
+++ wol/wakeOnLan.go
@@ -7,45 +7,66 @@ import (
 	"net"
 )
 
-func Wake(mac string, broadcasts []net.IP) error {
+func Wake(mac string, broadcasts []net.IP, port int) error {
 	if broadcasts == nil || len(broadcasts) == 0 {
 		return fmt.Errorf("no broadcast addresses specified")
 	}
-
-	mp, err := wol.New(mac)
+	bs, err := newMagicPacket(mac)
 	if err != nil {
-		return fmt.Errorf("failed to create magic packet: %w", err)
-	}
-
-	bs, err := mp.Marshal()
-	if err != nil {
-		return fmt.Errorf("failed to marshal magic packet: %w", err)
+		return err
 	}
 
 	for _, broadcast := range broadcasts {
 		if broadcast == nil {
 			return fmt.Errorf("the broadcast address cannot be nil")
 		}
-
-		conn, err := net.DialUDP(
-			"udp",
-			&net.UDPAddr{IP: nil},
-			&net.UDPAddr{IP: broadcast, Port: 9})
-
+		err = sendWoLPacket(broadcast, port, bs)
 		if err != nil {
-			log.Printf("failed to dial UDP: %s", err)
-			continue
+			return fmt.Errorf("failed to send WoL packet: %w", err)
 		}
-		defer conn.Close()
-		write, err := conn.Write(bs)
-		if err == nil && write != 102 {
-			err = fmt.Errorf("magic packet sent was %d bytes (expected 102 bytes sent)", write)
-		}
-		if err != nil {
-			log.Printf("failed to write UDP: %s", err)
-			continue
-		}
+
 		log.Printf("sent WoL packet to %s", broadcast)
+	}
+	return nil
+}
+
+func newMagicPacket(mac string) ([]byte, error) {
+	mp, err := wol.New(mac)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create magic packet: %w", err)
+	}
+
+	bs, err := mp.Marshal()
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal magic packet: %w", err)
+	}
+	return bs, nil
+}
+
+func sendWoLPacket(ip net.IP, port int, bs []byte) error {
+	if net.ParseIP(ip.String()) == nil {
+		return fmt.Errorf("the broadcast address cannot be nil or empty")
+	}
+
+	conn, err := net.DialUDP(
+		"udp",
+		&net.UDPAddr{IP: nil},
+		&net.UDPAddr{IP: ip, Port: port})
+
+	if err != nil {
+		return fmt.Errorf("failed to dial UDP: %w", err)
+	}
+
+	defer conn.Close()
+	log.Printf("sending magic packet to %s:%d\n", ip, port)
+
+	write, err := conn.Write(bs)
+	if err == nil && write != 102 {
+		err = fmt.Errorf("magic packet sent was %d bytes (expected 102 bytes sent)", write)
+	}
+
+	if err != nil {
+		return err
 	}
 	return nil
 }
