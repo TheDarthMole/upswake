815e1f93bd5d210f7f8de45c504300c522ab4088
Nick Ruffles <nick.ruffles@engineerbetter.com>
2023-10-21 22:31:26 +0100

    refactor: big refactor of WoL package __DESCRIPTION:__ Refactored WoL package to test package better. Removed attempt at mocking UDP connection and replaced with interface mocking

--- wol/wakeOnLan.go
+++ wol/wakeOnLan.go
@@ -3,32 +3,31 @@ package wol
 import (
 	"fmt"
 	"github.com/sabhiram/go-wol/wol"
-	"log"
+	"io"
 	"net"
 )
 
 const MagicPacketSize = 102
 
-func Wake(mac string, broadcasts []net.IP, port int) error {
-	if broadcasts == nil || len(broadcasts) == 0 {
-		return fmt.Errorf("no broadcast addresses specified")
-	}
-	bs, err := newMagicPacket(mac)
+func Wake(dst *net.UDPConn, mac string) error {
+	return wakeInternal(dst, mac)
+}
+
+func wakeInternal(dst io.ReadWriteCloser, mac string) error {
+	mp, err := newMagicPacket(mac)
 	if err != nil {
 		return err
 	}
 
-	for _, broadcast := range broadcasts {
-		if broadcast == nil {
-			return fmt.Errorf("the broadcast address cannot be nil")
-		}
-		err = sendWoLPacket(broadcast, port, bs)
-		if err != nil {
-			return fmt.Errorf("failed to send WoL packet: %w", err)
-		}
-
-		log.Printf("sent magic packet to %s:%d", broadcast, port)
+	size, err := dst.Write(mp)
+	if err == nil && size != MagicPacketSize {
+		err = fmt.Errorf("magic packet sent was %d bytes (expected %d bytes sent)", size, MagicPacketSize)
 	}
+
+	if err != nil {
+		return fmt.Errorf("failed to send WoL packet: %w", err)
+	}
+
 	return nil
 }
 
@@ -43,32 +42,4 @@ func newMagicPacket(mac string) ([]byte, error) {
 		return nil, fmt.Errorf("failed to marshal magic packet: %w", err)
 	}
 	return bs, nil
-}
-
-func sendWoLPacket(ip net.IP, port int, bs []byte) error {
-	if net.ParseIP(ip.String()) == nil {
-		return fmt.Errorf("the broadcast address cannot be nil or empty")
-	}
-
-	conn, err := net.DialUDP(
-		"udp",
-		&net.UDPAddr{IP: nil},
-		&net.UDPAddr{IP: ip, Port: port})
-
-	if err != nil {
-		return fmt.Errorf("failed to dial UDP: %w", err)
-	}
-
-	defer conn.Close()
-	log.Printf("sending magic packet to %s:%d\n", ip, port)
-
-	write, err := conn.Write(bs)
-	if err == nil && write != MagicPacketSize {
-		err = fmt.Errorf("magic packet sent was %d bytes (expected 102 bytes sent)", write)
-	}
-
-	if err != nil {
-		return err
-	}
-	return nil
 }
