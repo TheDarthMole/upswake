e875897c6a74c76b81eb16d5fd22fff02f9bd324
Nick Ruffles <nick.ruffles@engineerbetter.com>
2023-11-05 23:01:14 +0000

    feat: add json command summary

--- cmd/json.go
+++ cmd/json.go
@@ -14,13 +14,11 @@ import (
 // jsonCmd represents the json command
 var jsonCmd = &cobra.Command{
 	Use:   "json",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Retrieve JSON from a NUT server",
+	Long: `Retrieve JSON from a NUT server and print it to stdout
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+This is useful for testing the connection to a NUT server
+and for creating rego rules for a WoL target`,
 	Run: func(cmd *cobra.Command, args []string) {
 		wolTarget := config.WoLTarget{
 			Name: "test",
