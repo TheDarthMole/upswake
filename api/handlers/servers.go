f5c8b18d96aa4cfd84724461baf57ab96c18005a
Nick Ruffles <nick.ruffles@engineerbetter.com>
2024-01-03 17:36:15 +0000

    feat: added wake functionality via api

--- api/handlers/servers.go
+++ api/handlers/servers.go
@@ -1,21 +1,36 @@
 package handlers
 
 import (
-	"github.com/TheDarthMole/UPSWake/api"
 	"github.com/TheDarthMole/UPSWake/config"
 	"github.com/TheDarthMole/UPSWake/util"
 	"github.com/TheDarthMole/UPSWake/wol"
 	"github.com/labstack/echo/v4"
-	"log"
-	"net"
 	"net/http"
 )
 
 type ServerHandler struct{}
 
 type WakeServerRequest struct {
-	Port      int    `json:"mac" validate:"omitempty,gte=1,lte=65535" default:"9"`
-	Broadcast string `json:"broadcast" validate:"omitempty,ip"`
+	Port      int    `json:"port" validate:"required,gte=1,lte=65535"`
+	Broadcast string `json:"broadcast" validate:"required,ip"`
+	Mac       string `json:"mac" validate:"required,mac"`
+}
+
+type BroadcastWakeRequest struct {
+	Port int    `json:"port" validate:"required,gte=1,lte=65535"`
+	Mac  string `json:"mac" validate:"required,mac"`
+}
+
+func NewWakeServerRequest() *WakeServerRequest {
+	return &WakeServerRequest{
+		Port: config.DefaultWoLPort,
+	}
+}
+
+func NewBroadcastWakeRequest() *BroadcastWakeRequest {
+	return &BroadcastWakeRequest{
+		Port: config.DefaultWoLPort,
+	}
 }
 
 func NewServerHandler() *ServerHandler {
@@ -24,74 +39,59 @@ func NewServerHandler() *ServerHandler {
 
 func (h *ServerHandler) Register(g *echo.Group) {
 	g.GET("", HandlerNotImplemented)
+	g.POST("/wake", h.WakeServer)
+	g.POST("/broadcastwake", h.BroadcastWakeServer)
 	g.GET("/:mac", HandlerNotImplemented)
-	g.POST("/:mac/wake", h.WakeServer)
 }
 
 func (h *ServerHandler) WakeServer(c echo.Context) error {
-	mac := c.Param("mac")
-	if _, err := net.ParseMAC(mac); err != nil {
-		return c.JSON(http.StatusBadRequest, api.ErrorMessage{
-			Error:   "Bad Request",
-			Message: err.Error(),
-		})
-	}
-
-	wsRequest := new(WakeServerRequest)
+	wsRequest := NewWakeServerRequest()
 	if err := c.Bind(wsRequest); err != nil {
-		return c.JSON(http.StatusBadRequest, api.ErrorMessage{
-			Error:   "Bad Request",
-			Message: err.Error(),
-		})
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
 	if err := c.Validate(wsRequest); err != nil {
-		log.Println(err)
-		return c.JSON(http.StatusBadRequest, api.ErrorMessage{
-			Error:   "Bad Request",
-			Message: err.Error(),
-		})
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
-	if wsRequest.Broadcast != "" {
-		wolClient := wol.NewWoLClient(config.TargetServer{
-			Mac:       mac,
-			Broadcast: wsRequest.Broadcast,
-			Port:      wsRequest.Port,
-		})
+	wolClient := wol.NewWoLClient(config.TargetServer{
+		Name:      "API Request",
+		Mac:       wsRequest.Mac,
+		Broadcast: wsRequest.Broadcast,
+		Port:      wsRequest.Port,
+	})
 
-		if err := wolClient.Wake(); err != nil {
-			return c.JSON(http.StatusInternalServerError, api.ErrorMessage{
-				Error:   "Internal Server Error",
-				Message: err.Error(),
-			})
-		}
-		return c.JSON(http.StatusOK, "OK")
+	if err := wolClient.Wake(); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusCreated, Response{Message: "Wake on LAN packet sent"})
+}
+
+func (h *ServerHandler) BroadcastWakeServer(c echo.Context) error {
+	wsRequest := NewBroadcastWakeRequest()
+	if err := c.Bind(wsRequest); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
 	}
 
+	if err := c.Validate(wsRequest); err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
 	broadcasts, err := util.GetAllBroadcastAddresses()
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, api.ErrorMessage{
-			Error:   "Internal Server Error",
-			Message: err.Error(),
-		})
+		return err
 	}
 
 	for _, broadcast := range broadcasts {
 		wolClient := wol.NewWoLClient(config.TargetServer{
-			Mac:       mac,
+			Name:      "API Request",
+			Mac:       wsRequest.Mac,
 			Broadcast: broadcast.String(),
-			Port:      9,
+			Port:      wsRequest.Port,
 		})
 
 		if err = wolClient.Wake(); err != nil {
-			log.Println(err)
-			return c.JSON(http.StatusInternalServerError, api.ErrorMessage{
-				Error:   "Internal Server Error",
-				Message: err.Error(),
-			})
+			return err
 		}
 	}
-
-	return c.String(http.StatusOK, "OK")
+	return c.JSON(http.StatusCreated, Response{Message: "Wake on LAN packets sent to all available broadcast addresses"})
 }
