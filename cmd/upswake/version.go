43dfc9b366b9948c85252e904f043f78e93460a6
Nick Ruffles <nick@ruffles.dev>
2025-10-03 17:53:49 +0100

    feat(cli): adds version sub-command to show version, date and commit sha

---
+++ cmd/upswake/version.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	Version    string
+	Commit     string
+	Date       string
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Show the version information",
+		Long:  `Shows the version, commit hash, and build date of the application`,
+		Run: func(cmd *cobra.Command, args []string) {
+			sugar.Infof("UPSWake version %s, commit %s, built at %s", Version, Commit, Date)
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
