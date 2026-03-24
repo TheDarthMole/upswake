dd7e9818af16f88ef042d78da37ad47a6086baa4
Nick Ruffles <nick@ruffles.dev>
2026-03-24 21:54:36 +0000

    fix(viper): adds json tags to config structs

--- internal/infrastructure/config/viper/entities.go
+++ internal/infrastructure/config/viper/entities.go
@@ -5,19 +5,19 @@ type Config struct {
 }
 
 type NutServer struct {
-	Name     string          `mapstructure:"name"`
-	Host     string          `mapstructure:"host"`
-	Username string          `mapstructure:"username"`
-	Password string          `mapstructure:"password"`
-	Targets  []*TargetServer `mapstructure:"targets"`
-	Port     int             `mapstructure:"port"`
+	Name     string          `mapstructure:"name" json:"name"`
+	Host     string          `mapstructure:"host" json:"host"`
+	Username string          `mapstructure:"username" json:"username"`
+	Password string          `mapstructure:"password" json:"password"`
+	Targets  []*TargetServer `mapstructure:"targets" json:"targets"`
+	Port     int             `mapstructure:"port" json:"port"`
 }
 
 type TargetServer struct {
-	Name      string   `mapstructure:"name"`
-	MAC       string   `mapstructure:"mac"`
-	Broadcast string   `mapstructure:"broadcast"`
-	Interval  string   `mapstructure:"interval" default:"15m"`
-	Rules     []string `mapstructure:"rules"`
-	Port      int      `mapstructure:"port" default:"9"`
+	Name      string   `mapstructure:"name" json:"name"`
+	MAC       string   `mapstructure:"mac" json:"mac"`
+	Broadcast string   `mapstructure:"broadcast" json:"broadcast"`
+	Interval  string   `mapstructure:"interval" json:"interval" default:"15m"`
+	Rules     []string `mapstructure:"rules" json:"rules"`
+	Port      int      `mapstructure:"port" json:"port" default:"9"`
 }
