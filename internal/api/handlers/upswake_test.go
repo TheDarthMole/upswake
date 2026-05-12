796ecc17f04538a4ed45ff61c724055df8307443
Nick Ruffles <nick@ruffles.dev>
2026-05-12 23:34:33 +0100

    test(handlers): adds gomock mocks for ups and rule repositories

--- internal/api/handlers/upswake_test.go
+++ internal/api/handlers/upswake_test.go
@@ -10,47 +10,18 @@ import (
 
 	"github.com/TheDarthMole/UPSWake/internal/api"
 	"github.com/TheDarthMole/UPSWake/internal/domain/entity"
-	"github.com/TheDarthMole/UPSWake/internal/domain/repository"
-	"github.com/TheDarthMole/UPSWake/internal/infrastructure/rules"
-	mockrepository "github.com/TheDarthMole/UPSWake/internal/mocks"
+	"github.com/TheDarthMole/UPSWake/internal/mocks"
 	"github.com/labstack/echo/v5"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 )
 
-type mockUPSRepo struct {
-	err  error
-	json string
-}
-
-func (m *mockUPSRepo) GetJSON(_ *entity.NutServer) (string, error) {
-	return m.json, m.err
-}
-
 func TestUPSWakeHandler_RunWakeEvaluation(t *testing.T) {
 	validMac, err := entity.NewMacAddress("00:11:22:33:44:55")
 	require.NoError(t, err)
 
-	regoAlwaysTrue := newMemFS(t, map[string][]byte{
-		"always_true.rego": []byte(`package upswake
-default wake := true`),
-	})
-	regoAlwaysFalse := newMemFS(t, map[string][]byte{
-		"always_true.rego": []byte(`package upswake
-default wake := false`),
-	})
-
-	alwaysTrueRuleRepo, err := rules.NewPreparedRepository(regoAlwaysTrue)
-	require.NoError(t, err)
-
-	alwaysFalseRuleRepo, err := rules.NewPreparedRepository(regoAlwaysFalse)
-	require.NoError(t, err)
-
 	const validJSON = `[{"Name":"test-ups","Description":"Unavailable","Master":false,"NumberOfLogins":0,"Clients":[],"Variables":[{"Name":"battery.charge","Value":100,"Type":"INTEGER","Description":"Battery charge (percent of full)","Writeable":false,"MaximumLength":0,"OriginalType":"NUMBER"},{"Name":"ups.status","Value":"OL","Type":"STRING","Description":"UPS status","Writeable":false,"MaximumLength":0,"OriginalType":"NUMBER"}]}]`
-
-	upsRepo := mockrepository.NewMockUPSRepository(gomock.NewController(t))
-	upsRepo.EXPECT().GetJSON(gomock.Any()).Return(validJSON, nil).AnyTimes()
 
 	validConfig := &entity.Config{
 		NutServers: []*entity.NutServer{
@@ -95,11 +66,23 @@ default wake := false`),
 		},
 	}
 
+	type upsRepository struct {
+		err   error
+		json  string
+		times int
+	}
+
+	type ruleRepository struct {
+		err     error
+		times   int
+		allowed bool
+	}
+
 	type fields struct {
 		cfg      *entity.Config
-		upsRepo  repository.UPSRepository
-		ruleRepo repository.RuleRepository
+		ruleRepo ruleRepository
 		body     string
+		upsRepo  upsRepository
 	}
 	type wantedResponse struct {
 		body       string
@@ -113,10 +96,18 @@ default wake := false`),
 		{
 			name: "invalid_request_body",
 			fields: fields{
-				cfg:      validConfig,
-				upsRepo:  upsRepo,
-				ruleRepo: alwaysTrueRuleRepo,
-				body:     `invalid json`,
+				cfg: validConfig,
+				upsRepo: upsRepository{
+					err:   nil,
+					json:  validJSON,
+					times: 0,
+				},
+				ruleRepo: ruleRepository{
+					allowed: true,
+					err:     nil,
+					times:   0,
+				},
+				body: `invalid json`,
 			},
 			wantedResponse: wantedResponse{
 				body:       `{"message":"failed to parse request body","woken":false}`,
@@ -126,10 +117,18 @@ default wake := false`),
 		{
 			name: "valid_request",
 			fields: fields{
-				cfg:      validConfig,
-				upsRepo:  upsRepo,
-				ruleRepo: alwaysTrueRuleRepo,
-				body:     `{"mac":"00:11:22:33:44:55"}`,
+				cfg: validConfig,
+				upsRepo: upsRepository{
+					err:   nil,
+					json:  validJSON,
+					times: 1,
+				},
+				ruleRepo: ruleRepository{
+					allowed: true,
+					err:     nil,
+					times:   1,
+				},
+				body: `{"mac":"00:11:22:33:44:55"}`,
 			},
 			wantedResponse: wantedResponse{
 				body:       `{"message":"Wake on LAN sent","woken":true}`,
@@ -139,10 +138,18 @@ default wake := false`),
 		{
 			name: "rule_evaluates_to_false",
 			fields: fields{
-				cfg:      validConfig,
-				upsRepo:  upsRepo,
-				ruleRepo: alwaysFalseRuleRepo,
-				body:     `{"mac":"00:11:22:33:44:55"}`,
+				cfg: validConfig,
+				upsRepo: upsRepository{
+					err:   nil,
+					json:  validJSON,
+					times: 1,
+				},
+				ruleRepo: ruleRepository{
+					allowed: false,
+					err:     nil,
+					times:   1,
+				},
+				body: `{"mac":"00:11:22:33:44:55"}`,
 			},
 			wantedResponse: wantedResponse{
 				body:       `{"message":"No rule evaluated to true","woken":false}`,
@@ -152,10 +159,18 @@ default wake := false`),
 		{
 			name: "mac_not_in_config",
 			fields: fields{
-				cfg:      validConfig,
-				upsRepo:  upsRepo,
-				ruleRepo: alwaysTrueRuleRepo,
-				body:     `{"mac":"99:11:22:33:44:44"}`,
+				cfg: validConfig,
+				upsRepo: upsRepository{
+					err:   nil,
+					json:  validJSON,
+					times: 1,
+				},
+				ruleRepo: ruleRepository{
+					allowed: false,
+					err:     nil,
+					times:   0,
+				},
+				body: `{"mac":"99:11:22:33:44:44"}`,
 			},
 			wantedResponse: wantedResponse{
 				body:       `{"message":"MAC address not found in the config","woken":false}`,
@@ -165,10 +180,18 @@ default wake := false`),
 		{
 			name: "invalid_broadcast_address",
 			fields: fields{
-				cfg:      invalidConfig,
-				upsRepo:  upsRepo,
-				ruleRepo: alwaysTrueRuleRepo,
-				body:     `{"mac":"00:11:22:33:44:55"}`,
+				cfg: invalidConfig,
+				upsRepo: upsRepository{
+					err:   nil,
+					json:  validJSON,
+					times: 1,
+				},
+				ruleRepo: ruleRepository{
+					allowed: true,
+					err:     nil,
+					times:   1,
+				},
+				body: `{"mac":"00:11:22:33:44:55"}`,
 			},
 			wantedResponse: wantedResponse{
 				body:       `{"message":"Failed to create target server: broadcast is invalid, must be an IP address","woken":false}`,
@@ -179,9 +202,13 @@ default wake := false`),
 			name: "failing_ups_repo",
 			fields: fields{
 				cfg: validConfig,
-				upsRepo: &mockUPSRepo{
-					json: "",
-					err:  errors.New("failing rule"),
+				upsRepo: upsRepository{
+					err:   errors.New("failing rule"),
+					json:  "",
+					times: 1,
+				},
+				ruleRepo: ruleRepository{
+					times: 0,
 				},
 				body: `{"mac":"00:11:22:33:44:55"}`,
 			},
@@ -193,9 +220,13 @@ default wake := false`),
 		{
 			name: "invalid_mac_address",
 			fields: fields{
-				cfg:     validConfig,
-				upsRepo: upsRepo,
-				body:    `{"mac":"invalid mac address"}`,
+				cfg: validConfig,
+				upsRepo: upsRepository{
+					err:   nil,
+					json:  validJSON,
+					times: 0,
+				},
+				body: `{"mac":"invalid mac address"}`,
 			},
 			wantedResponse: wantedResponse{
 				body:       `{"message":"MAC address is invalid","woken":false}`,
@@ -207,12 +238,20 @@ default wake := false`),
 	e.Validator = api.NewCustomValidator(t.Context())
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			mock := gomock.NewController(t)
+
+			upsRepo := mocks.NewMockUPSRepository(mock)
+			upsRepo.EXPECT().GetJSON(gomock.Any()).Return(tt.fields.upsRepo.json, tt.fields.upsRepo.err).Times(tt.fields.upsRepo.times)
+
+			ruleRepo := mocks.NewMockRuleRepository(mock)
+			ruleRepo.EXPECT().Evaluate(gomock.Any(), gomock.Any()).Return(tt.fields.ruleRepo.allowed, tt.fields.ruleRepo.err).Times(tt.fields.ruleRepo.times)
+
 			req := httptest.NewRequest(http.MethodPost, "/upswake", strings.NewReader(tt.fields.body))
 			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 
 			rec := httptest.NewRecorder()
 			c := e.NewContext(req, rec)
-			h := NewUPSWakeHandler(tt.fields.cfg, tt.fields.upsRepo, tt.fields.ruleRepo)
+			h := NewUPSWakeHandler(tt.fields.cfg, upsRepo, ruleRepo)
 
 			if assert.NoError(t, h.RunWakeEvaluation(c)) {
 				assert.JSONEq(t, tt.wantedResponse.body, rec.Body.String())
@@ -229,8 +268,8 @@ func TestUPSWakeHandler_Register(t *testing.T) {
 
 	mock := gomock.NewController(t)
 
-	upsRepo := mockrepository.NewMockUPSRepository(mock)
-	ruleRepo := mockrepository.NewMockRuleRepository(mock)
+	upsRepo := mocks.NewMockUPSRepository(mock)
+	ruleRepo := mocks.NewMockRuleRepository(mock)
 
 	h := NewUPSWakeHandler(config, upsRepo, ruleRepo)
 	h.Register(e.Group("/"))
@@ -370,8 +409,8 @@ func TestUPSWakeHandler_ListNutServerMappings(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			mock := gomock.NewController(t)
 
-			upsRepo := mockrepository.NewMockUPSRepository(mock)
-			ruleRepo := mockrepository.NewMockRuleRepository(mock)
+			upsRepo := mocks.NewMockUPSRepository(mock)
+			ruleRepo := mocks.NewMockRuleRepository(mock)
 
 			e := echo.New()
 			req := httptest.NewRequest(http.MethodGet, "/", http.NoBody)
