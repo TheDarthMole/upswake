93163cabc9e7b5c90caad9e99c0c7ef024fd19fd
Nick Ruffles <nick@ruffles.dev>
2026-09-09 22:55:13 +0100

    fix(logging): remove quote characters from logging

--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -1,13 +1,12 @@
 package logging
 
 import (
-	"strconv"
 	"strings"
 )
 
 func SanitizeString(input string) string {
 	// Replace any non-printable characters with an empty string
-	sanitised := strconv.QuoteToASCII(input)
+	sanitised := strings.ReplaceAll(input, `"`, "")
 	sanitised = strings.TrimSpace(sanitised)
 	sanitised = strings.ReplaceAll(sanitised, "\n", "")
 	sanitised = strings.ReplaceAll(sanitised, "\r", "")
